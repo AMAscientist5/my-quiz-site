38ab30f779c0ff2087152dda0f7bc58811f709f5
Ahmad Musa <amaahmadmusa@gmail.com>
2022-10-11 04:05:16 -0700

    navber responsive

--- my-quiz-site/src/components/Header/Header.js
+++ my-quiz-site/src/components/Header/Header.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './Header.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+
 
 const Header = () => {
     return (
-        <nav className='header'>
-           <div>
-             <h1>My Quiz Site</h1>
-           </div>
-           <div className='nav'>
-            <Link to='/'>Home</Link>
-            <Link to='/statistics'>statistics</Link>
+        <div>
+     <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <div>
+        <Navbar.Brand href="/">My Quiz Site</Navbar.Brand>
+        </div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto custom">
+            <Link to='/'>Home</Link> 
+            <Link to='/statistics'>Statictics</Link>
             <Link to='/blog'>Blog</Link>
-           </div>
-        </nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        </div>
     );
 };
 
